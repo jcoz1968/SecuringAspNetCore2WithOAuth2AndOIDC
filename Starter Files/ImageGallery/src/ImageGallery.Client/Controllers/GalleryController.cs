d328345d40365df722631a153b96d1cc2c5b4c68
Jeffery Cosby <jcoz1968@hotmail.com>
2018-07-07 18:54:57 -0500

    Manually calling UserInfo endpoint

--- Starter Files/ImageGallery/src/ImageGallery.Client/Controllers/GalleryController.cs
+++ Starter Files/ImageGallery/src/ImageGallery.Client/Controllers/GalleryController.cs
@@ -1,4 +1,5 @@
-﻿using ImageGallery.Client.Services;
+﻿using IdentityModel.Client;
+using ImageGallery.Client.Services;
 using ImageGallery.Client.ViewModels;
 using ImageGallery.Model;
 using Microsoft.AspNetCore.Authentication;
@@ -183,6 +184,25 @@ namespace ImageGallery.Client.Controllers
         {
             await HttpContext.SignOutAsync("Cookies");
             await HttpContext.SignOutAsync("oidc");
+        }
+
+        public async Task<IActionResult> OrderFrame()
+        {
+            var discoveryClient = new DiscoveryClient("https://localhost:44379/");
+            var metaDataResponse = await discoveryClient.GetAsync();
+
+            var userInfoClient = new UserInfoClient(metaDataResponse.UserInfoEndpoint);
+
+            var accessToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+            var response = await userInfoClient.GetAsync(accessToken);
+
+            if(response.IsError)
+            {
+                throw new Exception("Problem accessing UserInfo endpoint.", response.Exception);
+            }
+
+            var address = response.Claims.FirstOrDefault(c => c.Type == "address")?.Value;
+            return View(new OrderFrameViewModel(address));
         }
     }
 }
