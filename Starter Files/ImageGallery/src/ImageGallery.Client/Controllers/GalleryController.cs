7aa912c9b47609fb1f31df7bb0d36342cfcdf412
Jeffery Cosby <jcoz1968@hotmail.com>
2018-07-07 17:22:09 -0500

    logout

--- Starter Files/ImageGallery/src/ImageGallery.Client/Controllers/GalleryController.cs
+++ Starter Files/ImageGallery/src/ImageGallery.Client/Controllers/GalleryController.cs
@@ -178,5 +178,11 @@ namespace ImageGallery.Client.Controllers
                 Debug.WriteLine($"Claim type: {claim.Type} - Claim value: {claim.Value}");
             }
         }
+
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync("Cookies");
+            await HttpContext.SignOutAsync("oidc");
+        }
     }
 }
