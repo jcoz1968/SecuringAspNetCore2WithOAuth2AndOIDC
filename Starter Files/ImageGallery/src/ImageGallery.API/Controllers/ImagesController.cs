46d73a8818f298d5c3275f0a70a81bf3dd80ea8d
Jeffery Cosby <jcoz1968@hotmail.com>
2018-07-08 16:55:16 -0500

    cleanup

--- Starter Files/ImageGallery/src/ImageGallery.API/Controllers/ImagesController.cs
+++ Starter Files/ImageGallery/src/ImageGallery.API/Controllers/ImagesController.cs
@@ -12,7 +12,6 @@ using System.IO;
 namespace ImageGallery.API.Controllers
 {
     [Route("api/images")]
-    [Authorize]
     public class ImagesController : Controller
     {
         private readonly IGalleryRepository _galleryRepository;
